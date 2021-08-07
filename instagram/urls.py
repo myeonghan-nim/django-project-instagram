b3c0b943966a664511f81f3aace35bebc61218db
myeonghan-nim <ataraxia9408@gmail.com>
2021-08-07 15:50:54 +0900

    Change: project name

--- instagram/urls.py
+++ instagram/urls.py
@@ -1,4 +1,4 @@
-"""insta URL Configuration
+'''insta URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -12,17 +12,15 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+'''
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from django.conf.urls.static import static
-from django.conf import settings
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
-    path('feeds/', include('feeds.urls')),
+    path('feed/', include('feed.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
